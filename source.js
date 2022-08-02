8c77f09dbce9e14c0db0dc3965ea337e4bd8c9a0
Meryem Yuksel <myuksel@ufl.edu>
2022-08-01 22:36:53 -0400

    can color for a 16*16 grid

--- source.js
+++ source.js
@@ -2,13 +2,17 @@
 const board = document.querySelector('#coloringboard');
 for(let i =0; i< 16; i++){
     const row = document.createElement('div');
+    row.classList.add('gridrow');
     board.appendChild(row);
   for(let j=0; j<16; j++){
         let square = document.createElement('div');
         square.classList.add('square');
-       square.style.top = 20*i+`px`;
-        square.style.left = 100*j+`px`;
-        square.textContent = "yo?";
+        square.addEventListener('mouseover',function(e){
+            if(e.buttons){
+            square.style.backgroundColor = 'black';
+            square.style.border = 'solid black';
+            }
+        });
         row.appendChild(square);
  }
 } 
